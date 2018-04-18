c282b3c6ab5460594d63b80646622ce967c5c161
Voronov Mikhail <michail.vms@yandex.ru>
2018-04-18 23:44:13 +0300

    add the first version of VolumeGroup reprents directory

---
+++ lib/yet_another_storage/src/volume_group/VolumeGroup.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace yas {
+namespace mount {
+
+// represent directory contains priority queue of physical volumes that mountes to it
+// and dispatch according priority
+class VolumeGroup {
+ public:
+ private:
+
+};
+
+} // namespace mount
+} // namespace yas
