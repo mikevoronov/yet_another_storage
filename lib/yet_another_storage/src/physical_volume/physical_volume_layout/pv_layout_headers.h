599e2c75347d5a56bbb5f6f6e820422ca7afe067
Voronov Mikhail <michail.vms@yandex.ru>
2018-04-19 14:12:20 +0300

    add the first version of physical volume main header

---
+++ lib/yet_another_storage/src/physical_volume/physical_volume_layout/pv_layout_headers.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "../../common/common.h"
+#include "../../common/settings.hpp"
+#include "pv_version.h"
+#include <cstdint>
+
+using namespace yas::macros;
+
+namespace yas {
+namespace pv_layout_headers {
+
+STRUCT_PACK(
+struct PVHeader {
+  uint8_t signature[] = { 'Y', 'A', 'S', '_', 'P', 'V' };                         //  + 6 bytes
+  pv_version version_;              // should move to class with operator <           + 2 bytes 
+  uint64_t pv_size_;                                                              //  + 8 bytes
+  uint32_t cluster_size_;                                                         //  + 4 bytes
+  uint32_t priority_;               // should be class (?)                            + 4 bytes
+  OffsetType root_entry_offset_;                                                  //  + 8 bytes
+  // freelists
+  //
+  // some index
+  //
+  // data
+
+});
+
+// TODO : add static assert to size of PVHeader
+
+} // namespace pv_layout_types_headers
+} // namespace yas
---
+++ lib/yet_another_storage/src/physical_volume/physical_volume_layout/pv_version.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstdint>
+
+namespace yas {
+
+struct pv_version {
+  std::uint8_t major;
+  std::uint8_t minor;
+};
+
+} // namespace yas
