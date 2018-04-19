ebee41562904d9fb6c1051eedd8d9c34823512ff
Voronov Mikhail <michail.vms@yandex.ru>
2018-04-19 19:25:23 +0300

    Add the first version of type header in physical volume. I assume that the most common types would be types with 4 and 8 bytes size. So there are specially size-optimized header for them. Each header could be in 2 states: allocated and freed. Allocated headers contain expired_time and data. Freed headers contain link instead of data. This link points to next freed header with the same size. This freelists then would used for allocate new entry in file (to decrease fragmentation and expensive extension of the physical volume on hdd). The idea stolen from (dl)ptmalloc fastbin realization.

---
+++ lib/yet_another_storage/src/physical_volume/physical_volume_layout/pv_layout_types_headers.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "../../common/common.h"
+#include "../../common/settings.hpp"
+#include <cstdint>
+
+using namespace yas::macros;
+
+namespace yas {
+namespace pv_layout_types_headers {
+
+enum ValueType : uint16_t {
+  kInt8 = 0,
+  kUint8 = 1,
+  kInt16 = 2,
+  kUint16 = 3,
+  kInt32 = 4,
+  kUint32 = 5,
+  kFloat = 6,
+  kDouble = 7,
+  kInt64 = 8,
+  kUint64 = 9,
+  kString = 10,
+  kBlob = 11,
+
+  kEmpty = 0x7FFF,        // high bit to determine is this value expired or not (tradeoff (types count)/performance
+                          // because of expired_time_high - 2 bytes aligment)
+};
+
+STRUCT_PACK(
+template <typename T>
+struct CommonTypeHeader {
+  ValueType value_type_;
+  uint16_t expired_time_high;    // goodbye 2038 problem :)
+  union {
+    struct {
+      uint32_t expired_time_low;
+      T value;
+    };
+    OffsetType next_free_entry_offset_;
+  };
+});
+
+STRUCT_PACK(
+struct Simple4TypeHeader {           // 12 bytes (follow the requirements of the lowest physical volume size)
+  CommonTypeHeader<uint32_t> body_;
+});
+
+STRUCT_PACK(
+struct Simple8TypeHeader {           // 16 bytes (follow the requirements of the lowest physical volume size)
+  CommonTypeHeader<uint64_t> body_;
+});
+
+STRUCT_PACK(
+struct ComplexTypeHeader {
+  ValueType value_type_;
+  uint16_t expired_time_high;
+  uint32_t expired_time_low;
+  union {
+    OffsetType next_free_entry_offset_;
+    uint8_t data[1];
+  };
+});
+
+} // namespace pv_layout_types_headers
+} // namespace yas
