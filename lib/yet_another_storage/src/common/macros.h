2376de3a3d6c19fd40d2875795ea806d774327b5
Voronov Mikhail <michail.vms@yandex.ru>
2018-04-22 21:53:06 +0300

    add static_assert on case of different compiler than VS and GCC

--- lib/yet_another_storage/src/common/macros.h
+++ lib/yet_another_storage/src/common/macros.h
@@ -9,6 +9,8 @@ __pragma(pack(push, 1)) __VA_ARGS__ __pragma(pack(pop))
 #elif defined(__GNUC__)
 #define STRUCT_PACK(...) \
 __VA_ARGS__ __attribute__((packed))
+#elif 
+static_assert(false, "You used not VS and GCC compiler - please add STRUCT_PACK by supported way of your compiler");
 #endif 
 
 } // namespace macros
