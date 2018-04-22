224167c059d4f10ea3c36b6cca1528f6deb36a64
Voronov Mikhail <michail.vms@yandex.ru>
2018-04-22 21:59:36 +0300

    add some comment

--- lib/yet_another_storage/src/common/macros.h
+++ lib/yet_another_storage/src/common/macros.h
@@ -9,7 +9,9 @@ __pragma(pack(push, 1)) __VA_ARGS__ __pragma(pack(pop))
 #elif defined(__GNUC__)
 #define STRUCT_PACK(...) \
 __VA_ARGS__ __attribute__((packed))
-#elif 
+#elif
+// more sutable would be generate smth like static warning to notify user of this library
+// but all realizations i have seen check compilers itself
 static_assert(false, "You used not VS and GCC compiler - please add STRUCT_PACK by supported way of your compiler");
 #endif 
 
