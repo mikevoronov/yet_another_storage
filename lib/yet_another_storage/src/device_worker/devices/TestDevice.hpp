eb0b1bdaf76e9de85bd6fa4076c74564a007f5a0
Voronov Mikhail <michail.vms@yandex.ru>
2018-05-24 01:57:54 +0300

    add TestDevice: - based on byte vector and have some covinent methods to manipulate it - will be used to unit test some functionalities of PVDeviceWorker and Storage to prevent file writing to filesystem

---
+++ lib/yet_another_storage/src/device_worker/devices/TestDevice.hpp
@@ -0,0 +1,105 @@
+#pragma once
+#include "../../common/filesystem.h"
+#include "../../common/common.h"
+#include "../../exceptions/YASException.h"
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+
+namespace yas {
+namespace devices {
+
+// this simple device based on std::vector
+template <typename OffsetType>
+class TestDevice {
+ public:
+  explicit TestDevice(fs::path path)
+  {}
+
+  ~TestDevice()
+  {}
+
+  TestDevice(const TestDevice &other)
+  {}
+
+  ByteVector Read(uint64_t position, uint64_t data_size) {
+    // possible overflow but it is just a device for test
+    if ((position + data_size) < storage_.size()) {
+      throw(exception::YASException("The device's get cursor position mismath", StorageError::kDeviceReadError));
+    }
+
+    auto begin = std::cbegin(storage_);
+    auto end = std::cbegin(storage_);
+    std::advance(begin, position);
+    std::advance(end, position + data_size);
+
+    return { begin, end };
+  }
+
+  template<typename Iterator>
+  uint64_t Write(uint64_t position, const Iterator begin, const Iterator end) {
+    if (position > storage_.size()) {
+      throw(exception::YASException("The device hasn't been opened during write", StorageError::kDeviceWriteError));
+    }
+
+    const auto data_size = std::distance(begin, end);
+    storage_.reserve(position + data_size);
+    if (position == storage_.size()) {
+      for (auto it = begin; it != end; ++it) {
+        storage_.push_back(*it);
+      }
+      return data_size;
+    }
+
+    auto storage_begin = std::begin(storage_);
+    auto storage_end = std::cbegin(storage_);
+    std::advance(storage_begin, position);
+    std::advance(storage_end, ((position + data_size) > storage_.size() ? storage_.size() : position + data_size));
+
+    auto data_it = begin;
+    for (; storage_begin != storage_end; ++storage_begin) {
+      *storage_begin = *data_it;
+      ++data_it;
+    }
+
+    if (storage_end != std::cend(storage_)) {
+      return data_size;
+    }
+
+    for (; data_it != end; ++data_it) {
+      storage_.push_back(*data_it);
+    }
+
+    return data_size;
+  }
+
+  bool IsOpen() const noexcept {
+    return true;
+  }
+
+  bool Close() noexcept {
+    return true;
+  }
+
+  ByteVector GetStorageContent() const {
+    return storage_;
+  }
+
+  template<typename Iterator>
+  void SetStorageContent(Iterator begin, Iterator end) {
+    ByteVector tmp;
+    storage_.swap(tmp);     // to reduce capacity
+
+    storage_.assign(begin, end);
+  }
+
+  TestDevice operator=(const TestDevice&) = delete;
+  TestDevice operator=(TestDevice&&) = delete;
+  TestDevice(TestDevice&&) = delete;
+ 
+ private:
+  ByteVector storage_;
+};
+
+} // namespace devices
+} // namespace yas
