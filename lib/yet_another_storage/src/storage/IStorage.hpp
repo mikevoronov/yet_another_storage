fdd2c3f0ee930caa150a835d2f23c8e19517c35e
Voronov Mikhail <michail.vms@yandex.ru>
2018-05-27 20:15:28 +0300

    IStorage::Get, IStorage:HasKey const removed: it provides more flexbility to inherited classes. And actually these methods could be not "logically" const because of they can f.e. collect statistics of using or update some db internal euristics

--- lib/yet_another_storage/src/storage/IStorage.hpp
+++ lib/yet_another_storage/src/storage/IStorage.hpp
@@ -13,8 +13,8 @@ class IStorage {
  public:
   using key_type = std::basic_string_view<CharType>;
   virtual nonstd::expected<bool,     StorageErrorDescriptor> Put(key_type key, std::any value) = 0;
-  virtual nonstd::expected<std::any, StorageErrorDescriptor> Get(key_type key) const = 0;
-  virtual nonstd::expected<bool,     StorageErrorDescriptor> HasKey(key_type key) const = 0;
+  virtual nonstd::expected<std::any, StorageErrorDescriptor> Get(key_type key) = 0;
+  virtual nonstd::expected<bool,     StorageErrorDescriptor> HasKey(key_type key) = 0;
   virtual nonstd::expected<bool,     StorageErrorDescriptor> Delete(key_type key) = 0;
   virtual nonstd::expected<bool,     StorageErrorDescriptor> SetExpiredDate(key_type key, time_t expired) = 0;
   virtual nonstd::expected<time_t,   StorageErrorDescriptor> GetExpiredDate(key_type key) = 0;
