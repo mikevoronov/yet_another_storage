346c534b6f94d762ce8bc307c8bb6901542c3122
michailvoronov <michail.vms@yandex.ru>
2018-07-19 14:55:41 +0300

    fix bug with possible exception in noexcpt methods

--- include/storage/lib/exceptions/ExceptionHandler.hpp
+++ include/storage/lib/exceptions/ExceptionHandler.hpp
@@ -10,6 +10,8 @@ class ExceptionHandler {
  public:
    /// \brief convert the exception to StorageErrorDescriptor
    /// \param exception - the pointer to exception to convert
+   /// TODO (!) - this class is only ad-hoc solution now (yes, there are some ugly work with std::string in try-catch)
+   /// and should be refactored in version 1.4
    static storage::StorageErrorDescriptor Handle(std::exception_ptr exception) {
     try {
       std::rethrow_exception(exception);
@@ -18,15 +20,33 @@ class ExceptionHandler {
       return yas_exception.getError();
     }
     catch (const std::runtime_error &runtime_exception) {
-      return { runtime_exception.what(), storage::StorageError::kUnknownExceptionType };
+      try {
+        // additional try-catch is needed because of ctor of std::string can throw exception
+        return { runtime_exception.what(), storage::StorageError::kUnknownExceptionType };
+      }
+      catch (...) {
+        // note that in C++17 std::string() is noexcept
+        return { std::string(), storage::StorageError::kMemoryNotEnough };
+      }
     }
     catch (const std::bad_alloc &exception) {
-      return { exception.what(), storage::StorageError::kMemoryNotEnough };
-    }
+      try {
+        return { exception.what(), storage::StorageError::kMemoryNotEnough };
+      }
+      catch (...) {
+        return { std::string(), storage::StorageError::kMemoryNotEnough };
+      }
+   }
     catch (const std::exception &exception) {
-      return { exception.what(), storage::StorageError::kUnknownExceptionType };
-    }
+      try {
+        return { exception.what(), storage::StorageError::kUnknownExceptionType };
+      }
+      catch (...) {
+        return { std::string(), storage::StorageError::kMemoryNotEnough };
+      }
+   }
     catch (...) {
+      // since in C++17 std::string() is noexcept - there shouldn't additional try-catch block
       return { std::string(), storage::StorageError::kUnknownExceptionType };
     }
   }
